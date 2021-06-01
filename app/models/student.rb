0362af19af30de709311271e514511634d660cd8
xiangma0531 <shoma0531@outlook.com>
2021-06-01 11:27:11 +0900

    Studentモデル作成〜indexアクション定義
    モデル作成・テーブル作成・バリデーション設定・ストパラ設定・ルーティング設定・コントローラー生成・indexアクション定義(空)

---
+++ app/models/student.rb
@@ -0,0 +1,12 @@
+class Student < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :name
+    validates :grade
+    validates :team
+    validates :number
+  end
+end
