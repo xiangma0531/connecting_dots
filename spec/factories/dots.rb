8362df11b507074dd09b4bdd2c7a54be1c583385
xiangma0531 <shoma0531@outlook.com>
2021-06-01 15:52:31 +0900

    テストコード実装

--- spec/factories/dots.rb
+++ spec/factories/dots.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :dot do
-    
+    title { "タイトル" }
+    acted_at { Faker::Date.backward }
+    category { 1 }
+    sentence = Faker::Lorem.sentence
+    introduction { sentence }
+    effort { sentence }
+    study { sentence }
+    association :student
   end
 end
--- spec/models/dot_spec.rb
+++ spec/models/dot_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Dot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    student = FactoryBot.create(:student)
+    @dot = FactoryBot.build(:dot)
+  end
+
+  describe '新規投稿機能' do
+    context '新規投稿が保存されるとき' do
+      it 'すべての項目を正しく入力していれば保存される' do
+        expect(@dot).to be_valid
+      end
+    end
+
+    context '新規投稿が保存されないとき' do
+      it 'titleが空では保存されない' do
+        @dot.title = ''
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Title can't be blank")
+      end
+      
+      it 'acted_atが空では保存されない' do
+        @dot.acted_at = ''
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Acted at can't be blank")
+      end
+
+      it 'categoryが空では保存されない' do
+        @dot.category = nil
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'introductionが空では保存されない' do
+        @dot.introduction = ''
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Introduction can't be blank")
+      end
+
+      it 'effortが空では保存されない' do
+        @dot.effort = ''
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Effort can't be blank")
+      end
+
+      it 'studyが空では保存されない' do
+        @dot.study = ''
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Study can't be blank")
+      end
+
+      it '紐づくstudentがいなければ保存されない' do
+        @dot.student = nil
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Student must exist")
+      end
+    end
+  end
 end
